8ae9bc74826c409b5d009e8ef95df6768bf788e5
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-04 20:42:09 +0530

    Day0

--- DAY0.java
+++ DAY0.java
@@ -6,7 +6,15 @@ import java.util.*;
 public class DAY0 {
 
     public static void main(String[] args) {
+        String str = "New";
+        System.out.println(str);
+        str = "New World";
+        System.out.println(str);
         System.out.println("This is our first class");
     }
     
 }
+// it supports primitiv datatype... hence its not pure object oriented.
+// if they tend to use only wrapper classes, then we can say it is pure obj oriented
+// string is a primitive .. because it has a seperate class
+
