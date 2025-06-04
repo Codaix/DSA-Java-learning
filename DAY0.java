fa58992dadef6b17a56f3c7ee6983d269ff5085c
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-04 20:23:08 +0530

    DAY0

---
+++ DAY0.java
@@ -0,0 +1,12 @@
+package DSA_java_learning;
+    // boilerplate 
+// // eihter package or import statement
+import java.util.*;
+// // class name
+public class DAY0 {
+
+    public static void main(String[] args) {
+        System.out.println("This is our first class");
+    }
+    
+}
