79e8642a278b23b255c8a0da4cf50ed05199b737
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-05 23:18:34 +0530

    DAY0

--- DAY0.java
+++ DAY0.java
@@ -1,6 +1,7 @@
 package DSA_java_learning;
     // boilerplate 
-// // eihter package or import statement
+// // eihter package or import statement Pakcages only applicable when needed.
+//not every time its required to import a package
 import java.util.*;
 // // class name
 public class DAY0 {
@@ -16,5 +17,9 @@ public class DAY0 {
 }
 // it supports primitive datatype... hence its not pure object oriented.
 // if they tend to use only wrapper classes, then we can say it is pure obj oriented
-// string is a primitive .. because it has a seperate class
+// string is a primitive .. because it has a seperate class.
+
+
+
+
 
