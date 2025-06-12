77afe280b7d49eb43f1f317f211d9dcb7b4fd5ee
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-12 10:40:19 +0530

    Day5

--- DAY5.java
+++ DAY5.java
@@ -60,6 +60,42 @@ public class DAY5 {
     /* homework: use this same question and rather than using different arrays, use brute force to
      * flag the rows and column elements inside the matrix and covert it there only.
      */
+    int[][] convertMatrixNew(int[][] arr) {
+    int rows = arr.length;
+    int cols = arr[0].length;
+
+    // First pass: mark the elements to be zeroed
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (arr[i][j] == 0) {
+                // Mark the entire row
+                for (int k = 0; k < cols; k++) {
+                    if (arr[i][k] != 0) {
+                        arr[i][k] = -1;
+                    }
+                }
+                // Mark the entire column
+                for (int k = 0; k < rows; k++) {
+                    if (arr[k][j] != 0) {
+                        arr[k][j] = -1;
+                    }
+                }
+            }
+        }
+    }
+
+    // Second pass: set all marked elements to zero
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (arr[i][j] == -1) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    return arr;
+}
+
 
     
 }
