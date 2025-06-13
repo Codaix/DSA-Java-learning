524689c3f152d58dff8f1363b6106f4e92b6f332
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-13 11:24:42 +0530

    DAY7 constructor

--- DAY7.java
+++ DAY7.java
@@ -11,10 +11,50 @@ final class Animal{
 
 // this class can be called and object can be created of this but it cannot be 
 // inherited and cannot be modified in any sense.
-
+/*
 public class DAY7{
     public static void main(String[] args) {
         Animal a = new Animal();
         a.speak();
+    }
+}*/
+//constructor: special method which is automatically provided by the class if 
+//             not defined.
+// // properties:
+// Same name as class
+// no return type
+// it will be called only once in an object life time
+// i.i. at the time of object creation
+// used to initialise the class object
+
+//Types of constructors: * default/non parameterized
+//                       * parameterized
+//                       * copy constructor.
+
+class Student{
+    String name;
+    int age;
+    //Default
+    Student(){
+    }
+
+    //Parameterized 
+    Student(String name, int age){
+        name = this.name;
+        age = this.age;
+        System.out.println(this);
+    }
+
+    //copy Contructor: it is supposed to copy an object.
+    Student(Student s){
+        name = s.name;
+        age = s.age;
+    }
+
+}
+public class DAY7{
+    public static void main(String[] args) {
+        Student s = new Student("Pranav", 10);
+        System.out.println(s);
     }
 }
