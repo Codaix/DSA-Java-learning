0106968e5aea396a2c4d64094cb0da85e9fc1c4b
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-18 11:59:55 +0530

    Collection

---
+++ DAY10/Collection.java
@@ -0,0 +1,14 @@
+package DAY10;
+//collection framework: 
+// list set and map.
+// 1. List: It is an ordered collection of elements that allows duplicate elements.
+//    It is implemented by ArrayList, LinkedList, and Vector.
+//    ArrayList and Vectors are replica of arrays and LinkedList is replica of doubly Linked List.
+//    why do we need arraylist and vector when we got array?
+//    solution: because of synchronization issue.
+//    synchronization issue: array list are not synchronized, which means that they are not thread-safe.
+//    Vector is synchronized, which means that it is thread-safe. which in leyman means we can use vector when more than one people are using it at same time.
+
+public class Collection {
+    
+}
