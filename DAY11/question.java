9903e8053155a5b003fd634efad8ea34c0b9afe8
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-19 12:02:12 +0530

    Day11 Question

---
+++ DAY11/question.java
@@ -0,0 +1,9 @@
+package DAY11;
+/*Que:
+* we have integer array num[3,2,3] and an integer value = 2. we need to remove the occurrance of value from the array without using extra space.
+* and return the answer array.
+e.g. num = [3,2,3] and value = 2 => num = [3,3] and return the new array i.e. [3,3].
+*/
+public class question {
+    
+}
