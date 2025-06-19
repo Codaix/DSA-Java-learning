523fc5942fa9d2679cbf0ab25abf3a5e4505ebc6
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-19 10:37:15 +0530

    Collection

--- DAY10/Collection.java
+++ DAY10/Collection.java
@@ -4,11 +4,12 @@ package DAY10;
 // 1. List: It is an ordered collection of elements that allows duplicate elements.
 //    It is implemented by ArrayList, LinkedList, and Vector.
 //    ArrayList and Vectors are replica of arrays and LinkedList is replica of doubly Linked List.
+//    They both are dynamic in nature, which means that they can grow and shrink in size as per the requirement.    
 //    why do we need arraylist and vector when we got array?
 //    solution: because of synchronization issue.
-//    synchronization issue: array list are not synchronized, which means that they are not thread-safe.
-//    Vector is synchronized, which means that it is thread-safe. which in leyman means we can use vector when more than one people are using it at same time.
-
+//    synchronization issue: Vector are not synchronized, which means that they are not thread-safe.
+//    ArrayList is synchronized, which means that it is thread-safe. which in leyman means we can use vector when more than one people are using it at same time.
+// 
 public class Collection {
     
 }
