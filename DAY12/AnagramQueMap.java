cc72e29299e636e035a01294d6b561a45ea3b327
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-23 12:03:21 +0530

    AnagramQuestion Using Map

---
+++ DAY12/AnagramQueMap.java
@@ -0,0 +1,12 @@
+package DAY12;
+//solution using tree map
+import java.util.ArrayList;
+import java.util.TreeMap;    
+public class AnagramQueMap {
+    // Function to group anagrams together
+    // Time Complexity: O(n * m * log m) where n is the number of words and m is the maximum length of a word
+    // Space Complexity: O(n * m)
+ArrayList<ArrayList<String>> anagrams(String words[]) {
+        TreeMap<String, ArrayList<String>> map = new TreeMap<>();
+       }
+}
