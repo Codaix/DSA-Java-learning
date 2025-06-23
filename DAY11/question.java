1cc9dfcd27c17fb223ac9f095b7768f0ceb4aa98
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-23 10:28:01 +0530

    QuestionDay11

--- DAY11/question.java
+++ DAY11/question.java
@@ -5,5 +5,24 @@ package DAY11;
 e.g. num = [3,2,3] and value = 2 => num = [3,3] and return the new array i.e. [3,3].
 */
 public class question {
+    public static void main(String[] args) {
+        int[] num = {3,2,3};
+        int value = 2;
+        int j = 0; // pointer to keep track of the new array
+        for(int i = 0; i<num.length; i++){
+            if(num[i] != value){ // if the current element is not equal to the value to be removed
+                num[j] = num[i]; // copy the element to the new array
+                j++; // increment the pointer
+            }
+        }
+        // print the new array
+        System.out.print("New array after removing " + value + ": ");
+        for(int i = 0; i<j; i++){
+            System.out.print(num[i] + " "); // print the elements of the new array
+        }
+        System.out.println(); // print a new line for better readability
+        
+        
+    }
     
 }
