7b79b9e19acb76c47a751b0c27205ad0afa4e089
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-24 11:56:40 +0530

    InsertionSort

---
+++ DAY13/InsertionSort.java
@@ -0,0 +1,38 @@
+package DAY13;
+// it sorts elements from left to right.
+// it is a comparison sort.
+// it is an in-place sorting algorithm.
+// it is a stable sorting algorithm.
+// it has a time complexity of O(n^2) in the worst case.
+class algo{
+    int[] insertionSort(int arr[]){
+        for(int i = 1; i<arr.length; i++){
+            int key = arr[i];
+            int j = i-1;
+            if(arr[j] > key){
+                while(j >= 0 && arr[j] >key){
+                    arr[j+1] = arr[j];
+                    j--;// this may lead to an out of bounds exception if j becomes -1.
+                }
+                arr[j+1] = key;// This line places the key element in its correct position in the sorted part of the array. 
+                // This is the key element that is inserted into the sorted part of the array.
+
+            }
+
+        }
+        return arr;
+    }
+}
+public class InsertionSort {
+    public static void main(String[] args) {
+        algo obj = new algo();
+        int[] arr = {12, 11, 13, 5, 6};
+        obj.insertionSort(arr);
+        System.out.println("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    
+}
