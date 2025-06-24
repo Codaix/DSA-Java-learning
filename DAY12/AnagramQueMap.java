926a14417fa2f3fa89e9fd5b85ed3f5a082ee7ec
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-24 10:18:13 +0530

    AnagramQuestion

--- DAY12/AnagramQueMap.java
+++ DAY12/AnagramQueMap.java
@@ -6,7 +6,28 @@ public class AnagramQueMap {
     // Function to group anagrams together
     // Time Complexity: O(n * m * log m) where n is the number of words and m is the maximum length of a word
     // Space Complexity: O(n * m)
-ArrayList<ArrayList<String>> anagrams(String words[]) {
+    ArrayList<ArrayList<String>> anagrams(String words[]) {
         TreeMap<String, ArrayList<String>> map = new TreeMap<>();
+        for (String word : words) {
+            // Sort the characters of the word to create a key
+            char[] charArray = word.toCharArray();
+            java.util.Arrays.sort(charArray);
+            String key = new String(charArray);
+            
+            // If the key is not present in the map, create a new list
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            // Add the original word to the list corresponding to the sorted key
+            map.get(key).add(word);
+        }   
+        // Prepare the result list from the map values
+        ArrayList<ArrayList<String>> result = new ArrayList<>();    
+        for (ArrayList<String> anagramList : map.values()) {
+            result.add(anagramList);
+        }
+        return result;
+
+
        }
 }
