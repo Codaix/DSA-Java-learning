23313f74e9e6df9afe303522b36c088a728c2bfc
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-24 11:57:22 +0530

    SortingAlgorithms

---
+++ DAY13/SortingAlgo.java
@@ -0,0 +1,10 @@
+package DAY13;
+// important sorting algos:
+// 1. Bubble Sort : a.k.a. comparison sort
+// 2. Selection Sort
+// 3. Insertion Sort
+// 4. Merge Sort
+// 5. Quick Sort
+public class SortingAlgo {
+    
+}
