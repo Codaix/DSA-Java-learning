41903fddf1c1a8f55725dc0e2baf65406b84a475
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-24 11:56:55 +0530

    MergeSort

---
+++ DAY13/MergeSort.java
@@ -0,0 +1,11 @@
+package DAY13;
+// works on divide and conquer principle.
+// using recursoin to sort the array without using looping
+// three steps in recursion:
+// base case: when the array has only one element, it is already sorted.
+// recursive call: divide the array into two halves and sort each half.
+// our part: merge the two sorted halves into a single sorted array.
+//assignment: implement merge sort using recursion.
+public class MergeSort {
+    
+}
