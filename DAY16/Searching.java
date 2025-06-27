142cf19ffd2fd2f9367dbceda37b780e9b6a670e
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-27 11:58:13 +0530

    Searching Techniques

---
+++ DAY16/Searching.java
@@ -0,0 +1,18 @@
+package DAY16;
+// we have linear and binary searching techniques.
+// linear search technique checks for each element and return true and false based on the 
+// condition we have.
+
+// Binary Search: it will only work with sorted elements.
+// e.g.arr[] and we need to get if t is in the array or not. 
+// 1. we need to get the mid of the array.
+// 2. we need to check the condition whether the mid is equal to the target or not
+// 3. if unequal, we need to check if the target is smaller or greater than the mid element.
+// 4. if mid element is greater than the target, there is no need to search the elements to the left of the mid element.
+// 5. if mid element is samller, no need to check at the right hand side of the mid element.
+// 6. now, for each case, we need to repeat the step 1 till we hit the target.
+
+public class Searching {
+    
+    
+}
