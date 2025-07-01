a7e39874641e0c69a3f60b77b3efbe63ceef9563
Codaix <160418176+Codaix@users.noreply.github.com>
2025-07-01 21:06:16 +0530

    LinkedList

--- LinkedList.java
+++
@@ -1,7 +0,0 @@
-public class LinkedList {
-    // it is a linear data stucture... i.e. stores data one after another.
-    // elements are provided by reference to next node so as elements can retrieve other element
-    // []-----[]-----[]------[]
-    // assignment: implement linked list node
-    
-}
