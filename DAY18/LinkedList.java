a4c5efab4c7d23f6843bfeb29de98ffb9646a079
Codaix <160418176+Codaix@users.noreply.github.com>
2025-07-01 21:07:09 +0530

    LinkedList

--- DAY18/LinkedList.java
+++ DAY18/LinkedList.java
@@ -1,4 +1,6 @@
+package DAY18;
 public class LinkedList {
+
     // it is a linear data stucture... i.e. stores data one after another.
     // elements are provided by reference to next node so as elements can retrieve other element
     // []-----[]-----[]------[]
