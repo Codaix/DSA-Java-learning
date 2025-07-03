e324b9b6d4b81f223a2225847a4f251e91bd82aa
Codaix <160418176+Codaix@users.noreply.github.com>
2025-07-03 18:16:58 +0530

    Get Int At Target

--- DAY20/que.java
+++ DAY20/que.java
@@ -6,6 +6,46 @@ package DAY20;
 
 // my thought; get the length. divide by 2. if odd then return the ceil value.
 // make the value after divide as the target and check the list till the target and return the node at the target.
+class Node{
+    public int data;
+    public Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+        
+    }
+}
 public class que {
+    public static void main(System[] args){
+
+    }
+    public int Length(Node head){
+        if (head == null) return 0;
+        return 1 + Length(head.next);
+    }
+
+    public int mid(Node head){
+        int size = Length(head);
+        int midSize = 0;
+        if(size/2 != 0){
+            midSize = (size+1)/2;
+        }else{
+        midSize = size/2;
+        }
+        int target = midSize;
+        return target;
+    }
+    public Node getInt(Node head, int target){
+        Node start = head;
+        int count = 0;
+        while(start != null){
+            if(count == target){
+                return start;
+            }
+            start = start.next;
+            count++;
+        }
+        return null;
+    }
     
 }
