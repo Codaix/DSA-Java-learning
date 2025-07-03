df80d45ed7582c9cdbfa34f49d45d5c91911fe94
Codaix <160418176+Codaix@users.noreply.github.com>
2025-07-03 11:58:58 +0530

    pending

---
+++ DAY20/que.java
@@ -0,0 +1,11 @@
+package DAY20;
+// we have head of list. we need to return the middle of the list. 
+//if length is even, we need to return the second middle element. 
+//i.e. if 1,2,3,4 then return 3
+// if 5, 9, 9, 3, 5 return 9
+
+// my thought; get the length. divide by 2. if odd then return the ceil value.
+// make the value after divide as the target and check the list till the target and return the node at the target.
+public class que {
+    
+}
