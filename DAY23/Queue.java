a6cec32e7bd88e43fdc0e7411a37f74ee550ffd0
Codaix <160418176+Codaix@users.noreply.github.com>
2025-07-08 11:48:50 +0530

    Queue

---
+++ DAY23/Queue.java
@@ -0,0 +1,59 @@
+package DAY23;
+// implimenting push pop front empty and size using linked list.
+
+//importing the pre built node file;
+import java.util.*;
+
+class Node{
+    public int data;
+    public Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+class MyQueue {
+    private Node front, rear;
+    private int size;
+    public MyQueue() {
+        front = rear = null;
+        size = 0;
+    }
+    public Node push(int data){
+        Node newnode = new Node(data);
+        if(rear == null){
+            front = rear = newnode;
+        }
+        else{
+            rear.next = newnode;
+            rear = newnode;
+        }
+        size++;
+        return newnode;
+    }
+    public int pop() {
+        if (front == null) return -1;
+        int value = front.data;
+        front = front.next;
+        if (front == null) rear = null;
+        size--;
+        return value;
+    }
+    public int front() {
+        if (front == null) return -1;
+        return front.data;
+    }
+
+    public boolean empty() {
+        return front == null;
+    }
+
+    public int size() {
+        return size;
+    }
+}
+//implement this using stack.
+
+public class Queue {
+        
+}
