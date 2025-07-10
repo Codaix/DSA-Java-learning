23156a2f18c6e4830438e9da080cf5412d8498d2
Codaix <160418176+Codaix@users.noreply.github.com>
2025-07-10 11:00:49 +0530

    BTNode

---
+++ DAY25/BTNode.java
@@ -0,0 +1,13 @@
+package DAY25;
+
+public class BTNode{
+    public int val;
+    public BTNode left;
+    public BTNode right;
+
+    public BTNode(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
